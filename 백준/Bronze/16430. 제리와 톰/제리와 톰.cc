1d28272d9ca43d8733ae19bb540865a9fce92d64
W0NM2NJA2 <80443295+m2nja201@users.noreply.github.com>
2022-05-09 22:21:53 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -1,17 +1,10 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
 int main()
 {
-    ios_base::sync_with_stdio(0);
-    cout.tie(0); cin.tie(0);
-
     int A, B;
-    cin>>A>>B; 
-
-    int ans = 0;
-    ans = B - A;
-
-    cout<<ans <<" "<<B; 
+    scanf("%d %d", &A, &B);
+    
+    printf("%d %d", B - A, B);
     return (0);
 }
