96e6ee515b99beaf9c16d73a97cbd610a1a8b9ea
W0NM2NJA2 <80443295+m2nja201@users.noreply.github.com>
2022-05-24 22:25:22 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout.tie(0);
+    cin.tie(0);
+    
+    int n, k, a, b;
+    cin >> n >> k;
+    for (int i=0; i<n; i++){
+        cin >> a >> b;
+    }
+    cout<< "비와이";
+    return 0;
+}
